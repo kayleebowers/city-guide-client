2829acf999f5b91e5d49289e88eb8ee552347db4
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-27 19:34:16 +0000

    feat: add route to homepage

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,44 +1,56 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 export const MainView = () => {
-    const [ activities, setActivities ] = useState([]);
-    const [ user, setUser ] = useState(null);
-    
-    //assign server variable 
-    const server = "https://city-guide-api-3d2f74a4c59e.herokuapp.com";
+  const [activities, setActivities] = useState([]);
+  const [user, setUser] = useState(null);
 
-    //fetch all activities from api and assign them to activities state
-    fetch(`${server}/activities`)
-        .then((response) => response.json())
-        .then((activities) => {
-            const activitiesFromApi = activities.map((activity) => {
-                return {
-                    _id: activity._id,
-                    Name: activity.Name,
-                    Description: activity.Description,
-                    Type: activity.Type,
-                    ImagePath: activity.ImagePath,
-                    Price: activity.Price,
-                    Website: activity.Website,
-                    Address: activity.Address
-                };
-            });
-            setActivities(activitiesFromApi);
-        }).catch((error) => {
-            console.error(error);
-        });
+  //assign server variable
+  const server = "https://city-guide-api-3d2f74a4c59e.herokuapp.com";
+
+  //fetch all activities from api and assign them to activities state
+  fetch(`${server}/activities`)
+    .then((response) => response.json())
+    .then((activities) => {
+      const activitiesFromApi = activities.map((activity) => {
+        return {
+          _id: activity._id,
+          Name: activity.Name,
+          Description: activity.Description,
+          Type: activity.Type,
+          ImagePath: activity.ImagePath,
+          Price: activity.Price,
+          Website: activity.Website,
+          Address: activity.Address,
+        };
+      });
+      setActivities(activitiesFromApi);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
   return (
-    <>
-      <h1>Activities</h1>
-      {activities.map((activity) => {
-        return (
-            <div>
-                <p>{activity.Name}</p>
-            </div>
-        )
-      })}
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* route to homepage */}
+        <Route
+          path="/"
+          element={
+            <>
+              <h1>Activities</h1>
+              {activities.map((activity) => {
+                return (
+                  <div>
+                    <p>{activity.Name}</p>
+                  </div>
+                );
+              })}
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 };
