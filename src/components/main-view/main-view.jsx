851099478e88733ff042a280c705f003595a1660
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-27 19:01:32 +0000

    feat: fetch activities data from api

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 export const MainView = () => {
+    const [ activities, setActivities ] = useState([]);
+    const [ user, setUser ] = useState(null);
+    
+    //assign server variable 
+    const server = "https://city-guide-api-3d2f74a4c59e.herokuapp.com";
+
+    fetch(`server/activities`
+        .then((response) => response.json())
+        .then((activities) => {
+            const activitiesFromApi = activities.map((activity) => {
+                return {
+                    _id: activity._id,
+                    Name: activity.Name,
+                    Description: activity.Description,
+                    Type: activity.Type,
+                    ImagePath: activity.ImagePath,
+                    Price: activity.Price,
+                    Website: activity.Website,
+                    Address: activity.Address
+                };
+            });
+            setActivities(activitiesFromApi);
+        }).catch((error) => {
+            console.error(error);
+        })
+    );
+    
   return (
     <>
-      <h1>MainView</h1>
+      <h1>Pegasus City Guide</h1>
     </>
   );
 };
