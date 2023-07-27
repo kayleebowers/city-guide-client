3e7584157f704cb61f05a35a2fe54d6e20727616
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-27 22:19:40 +0000

    test: built arts type component

--- src/components/navigation-bar/activity-type/arts-view.jsx
+++ src/components/navigation-bar/activity-type/arts-view.jsx
@@ -1,3 +1,12 @@
-export const ArtsView = () => {
-    
+import { useParams } from "react-router";
+
+export const ArtsView = ({activities}) => {
+// const { type } = useParams();
+const arts = activities.filter((activity) => activity.Type === "Arts");
+console.log(arts);
+return (
+    <>
+        <h1>{arts}</h1>
+    </>
+)
 }
