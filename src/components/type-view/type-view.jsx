d7efad7b8b3c085e0d5b408387a81cb704fffebc
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-28 03:08:47 +0000

    feat: set up type-view carousel

--- src/components/type-view/type-view.jsx
+++ src/components/type-view/type-view.jsx
@@ -1,20 +1,29 @@
 import { useParams } from "react-router";
-import { Col } from "react-bootstrap";
-import { Carousel, ExampleCarouselImage } from 'react-bootstrap';
+import { Col, Row } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 
-export const TypeView = ({activities}) => {
-const { type } = useParams();
-const events = activities.filter((activity) => activity.Type === type);
-return (
-    <>
-        {events.map((event) => {
-        return (
-            <Col key={event._id}>
-            <h1>{event.Name}</h1>
-            </Col>
-        );
-    }
-    )}
-    </>
-)
-}
+export const TypeView = ({ activities }) => {
+  const { type } = useParams();
+  const events = activities.filter((activity) => activity.Type === type);
+  return (
+    <Col >
+      <Carousel style={{border: "1px solid red"}} className="w-50">
+        {events.map((slide) => {
+          return (
+            <Carousel.Item key={slide._id}>
+              <img
+                className="w-100"
+                src={slide.ImagePath}
+                alt="slider image"
+              />
+              <Carousel.Caption>
+                <h3>{slide.Name}</h3>
+                <p>{slide.Description}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    </Col>
+  );
+};
