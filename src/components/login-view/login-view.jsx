aad33e46331d9f9fc387defda8ae8394f1cf2098
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-31 20:03:03 +0000

    style: make login and sign up responsive

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,62 +1,90 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { useState } from 'react';
-import { useNavigate } from 'react-router';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { Row, Col } from "react-bootstrap";
 
 export const LoginView = ({ onLogin, server, user }) => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const handleLogin = (e) => {
-        e.preventDefault();
-        
-        //request being sent to api
-        const data = {
-            Username: username,
-            Password: password
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    //request being sent to api
+    const data = {
+      Username: username,
+      Password: password,
+    };
+
+    //get matching user info from api
+    fetch(`${server}/login`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      //get json object to extract user and JWT
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.user) {
+          //add data to local storage
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);
+          onLogin(data.user, data.token);
+          navigate("/users/:id");
+        } else {
+          alert("Account not found");
         }
+      })
+      .catch((error) => {
+        alert("Something went wrong");
+        console.error(error);
+      });
+  };
 
-        //get matching user info from api 
-        fetch(`${server}/login`, {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        //get json object to extract user and JWT
-        .then((response) => response.json())
-        .then((data) => {
-            if (data.user) {
-                //add data to local storage
-                localStorage.setItem("user", JSON.stringify(data.user));
-                localStorage.setItem("token", data.token)
-                onLogin(data.user, data.token);
-                navigate("/users/:id");
-            } else {
-                alert("Account not found");
-            }
-        }).catch((error) => {
-            alert("Something went wrong");
-            console.error(error);
-        })
-    }    
-
-    return (
-        <Form onSubmit={handleLogin}>
+  return (
+    <Col
+      lg={6}
+      md={8}
+      sm={10}
+      className="mx-auto p-5 d-flex flex-column"
+      style={{ border: "1px solid red" }}
+    >
+      <div className="text-center">
+        <h1 className="mx-auto mb-4">Login</h1>
+      </div>
+      <Form
+        onSubmit={handleLogin}
+        className="d-flex flex-column align-content-center justify-content-center"
+      >
         <Form.Group className="mb-3" controlId="formUsername">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="text" required minLength={5} value={username} onChange={(e) => setUsername(e.target.value)}/>
+          <Form.Control
+            type="text"
+            required
+            minLength={5}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </Form.Group>
-  
+
         <Form.Group className="mb-3" controlId="formPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" required minLength={5} value={password} onChange={(e) => setPassword(e.target.value)} />
+          <Form.Control
+            type="password"
+            required
+            minLength={5}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" className="mx-auto mt-2">
           Login
         </Button>
-      </Form> 
-    )
-}
+      </Form>
+    </Col>
+  );
+};
--- src/components/sign-up-view/sign-up-view.jsx
+++ src/components/sign-up-view/sign-up-view.jsx
@@ -1,63 +1,97 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import { useState } from "react";
-import { useNavigate } from 'react-router';
+import { useNavigate } from "react-router";
+import { Row, Col } from "react-bootstrap";
 
-export const SignUpView = ({server}) => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
+export const SignUpView = ({ server }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSignUp = (e) => {
-        e.preventDefault();
+  const handleSignUp = (e) => {
+    e.preventDefault();
 
-        //define request body
-        const data = {
-            Username: username,
-            Password: password,
-            Email: email
+    //define request body
+    const data = {
+      Username: username,
+      Password: password,
+      Email: email,
+    };
+
+    //Post to api to create new user
+    fetch(`${server}/users`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert("Sign up successful");
+          navigate("/login");
+        } else {
+          alert("Sign up failed");
         }
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("Something went wrong");
+      });
+  };
 
-        //Post to api to create new user
-        fetch(`${server}/users`, {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }).then((response) => {
-            if (response.ok) {
-                alert('Sign up successful');
-                navigate("/login");
-            } else {
-                alert("Sign up failed");
-            }
-        }).catch((error) => {
-            console.error(error);
-            alert("Something went wrong");
-        })
-    }
-
-    return (
-        <Form onSubmit={handleSignUp}>
+  return (
+    <Col
+      lg={6}
+      md={8}
+      sm={10}
+      className="mx-auto p-5"
+      style={{ border: "1px solid red" }}
+    >
+      <div className="text-center">
+        <h1 className="mx-auto mb-4">Sign up to track your activities</h1>
+      </div>
+      <Form
+        onSubmit={handleSignUp}
+        className="d-flex flex-column justify-content-center"
+      >
         <Form.Group className="mb-3" controlId="formUsername">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="text" required minLength={5} value={username} onChange={(e) => setUsername(e.target.value)}/>
+          <Form.Control
+            type="text"
+            required
+            minLength={5}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </Form.Group>
-  
+
         <Form.Group className="mb-3" controlId="formPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" required minLength={5} value={password} onChange={(e) => setPassword(e.target.value)} />
+          <Form.Control
+            type="password"
+            required
+            minLength={5}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formEmail">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+          <Form.Control
+            type="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" className="mx-auto mt-2">
           Sign up
         </Button>
-      </Form> 
-    )
-}
+      </Form>
+    </Col>
+  );
+};
