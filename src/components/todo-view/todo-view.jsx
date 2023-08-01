96bcf554b180c840cb406f5d981e10b0197a6166
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-01 01:03:09 +0000

    feat: add checkboxes to todo view

--- src/components/todo-view/todo-view.jsx
+++ src/components/todo-view/todo-view.jsx
@@ -1,16 +1,25 @@
 import Button from "react-bootstrap/Button";
+import { Col } from "react-bootstrap";
 
 export const Todo = ({ user, server, token, activities }) => {
+    //get array of activities that matches user.Todos
   let listItems = activities.filter((activity) =>
     user.Todos.includes(activity._id)
   );
-  console.log(listItems);
+
   return (
     <>
       <h3>Todo List</h3>
       <ul>
         {listItems.map((todo) => {
-          return <li key={todo._id}>{todo.Name}</li>;
+          return (
+            <>
+                <Col className="d-flex">
+                    <li key={todo._id}>{todo.Name}</li>
+                    <input type="checkbox"></input>
+                </Col>
+            </>
+          );
         })}
       </ul>
     </>
