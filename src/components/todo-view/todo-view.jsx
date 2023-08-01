4df83babfc00936121aed53529a87dfa39979f94
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-01 00:46:31 +0000

    feat: map todo list items on todo view

--- src/components/todo-view/todo-view.jsx
+++ src/components/todo-view/todo-view.jsx
@@ -1,18 +1,18 @@
 import Button from "react-bootstrap/Button";
 
-export const Todo = (({user, server, token, activities}) => {
-    let todos = user.Todos;
-    console.log(todos);
-    return (
-        <>
-            <h3>Todo List</h3>
-            {todos.map((todo) => {
-                return (
-                <ul>
-                    <li key={todo}>{todo}</li>
-                </ul>
-                )
-            })}
-        </>
-    )
-})
+export const Todo = ({ user, server, token, activities }) => {
+  let listItems = activities.filter((activity) =>
+    user.Todos.includes(activity._id)
+  );
+  console.log(listItems);
+  return (
+    <>
+      <h3>Todo List</h3>
+      <ul>
+        {listItems.map((todo) => {
+          return <li key={todo._id}>{todo.Name}</li>;
+        })}
+      </ul>
+    </>
+  );
+};
