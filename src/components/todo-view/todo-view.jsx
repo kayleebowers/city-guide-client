9bceaa26faa6612b8c6a37b61a2a839bde5d6d27
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-01 19:28:11 +0000

    feat: add todo and completed items to carousels

--- src/components/todo-view/todo-view.jsx
+++ src/components/todo-view/todo-view.jsx
@@ -35,38 +35,41 @@ export const Todo = ({ user, server, token, activities }) => {
   return (
     <>
       <h3>Todo List</h3>
-      {/* <Carousel style={{border: "1px solid red"}} className="w-100"> */}
-      <div
-        id="carouselExampleSlidesOnly"
-        className="carousel slide"
-        data-ride="carousel"
-      >
-        <div className="carousel-inner">
+      <Carousel style={{border: "1px solid red"}} className="w-50">
           {listItems.map((todo) => {
             return (
-              <>
-                <div className="carousel-item active">
-                  <ActivitiesCard activity={todo} />
-                </div>
-              </>
+              <Carousel.Item key={todo._id}>
+                <Carousel.Caption>
+                  <h3>{todo.Name}</h3>
+                </Carousel.Caption>
+                <img
+                  style={{ height: "60vh", width: "100%", objectFit: "cover"}}
+                  className="mh-10"
+                  src={todo.ImagePath}
+                  alt="slider image"
+                />
+              </Carousel.Item>
             );
           })}
-        </div>
-      </div>
-      {/* </Carousel> */}
+      </Carousel>
       <h3>Memories You've Made</h3>
-      <ul>
-        {memories.map((completed) => {
-          return (
-            <>
-              <Col className="d-flex">
-                <li key={completed._id}>{completed.Name}</li>
-                <input type="checkbox"></input>
-              </Col>
-            </>
-          );
-        })}
-      </ul>
+      <Carousel style={{border: "1px solid red"}} className="w-50">
+          {memories.map((completed) => {
+            return (
+              <Carousel.Item key={completed._id}>
+                <Carousel.Caption>
+                  <h3>{completed.Name}</h3>
+                </Carousel.Caption>
+                <img
+                  style={{ height: "60vh", width: "100%", objectFit: "cover"}}
+                  className="mh-10"
+                  src={completed.ImagePath}
+                  alt="slider image"
+                />
+              </Carousel.Item>
+            );
+          })}
+      </Carousel>
     </>
   );
 };
