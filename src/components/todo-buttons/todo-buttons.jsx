2e1735f9ba862df973107f632e318fd03d905d0b
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-02 18:15:33 +0000

    refactor: add todo buttons component

---
+++ src/components/todo-buttons/todo-buttons.jsx
@@ -0,0 +1,68 @@
+import Card from "react-bootstrap/Card";
+import { Col, Button } from "react-bootstrap";
+
+export const TodoButtons = ({
+  todo,
+  deleteTodo,
+  setClicked,
+  addToTodo,
+  addCompleted,
+  deleteCompleted,
+  user,
+  completed,
+}) => {
+  return (
+    <>
+      {/* if user, add card body */}
+      {user && (
+        <Card.Body className="d-flex align-items-center justify-content-center">
+          <>
+            {todo ? (
+              <Button
+                onClick={() => {
+                  deleteTodo();
+                  setClicked(false);
+                }}
+                className="py-2 mx-1"
+              >
+                Delete Todo
+              </Button>
+            ) : (
+              <Button
+                onClick={() => {
+                  addToTodo();
+                  setClicked(true);
+                }}
+                className="py-2 mx-1"
+              >
+                Add Todo
+              </Button>
+            )}
+
+            {!completed ? (
+              <Button
+                className="py-2 mx-1"
+                onClick={() => {
+                  addCompleted();
+                  setClicked(true);
+                }}
+              >
+                Add Memory
+              </Button>
+            ) : (
+              <Button
+                className="py-2 mx-1"
+                onClick={() => {
+                  deleteCompleted();
+                  setClicked(false);
+                }}
+              >
+                Remove Memory
+              </Button>
+            )}
+          </>
+        </Card.Body>
+      )}
+    </>
+  );
+};
