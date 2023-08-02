0c366f92e5274a7fd4f6cf7e4425cf40bd1c0dcf
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-08-02 16:29:07 +0000

    style: style badges/header in activity view

--- src/components/activity-view/activity-view.jsx
+++ src/components/activity-view/activity-view.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
+import Badge from "react-bootstrap/Badge";
 
 export const ActivityView = ({ activities }) => {
   //get route parameter
@@ -10,21 +11,23 @@ export const ActivityView = ({ activities }) => {
 
   return (
     <>
-      <Card className="d-flex flex-row-reverse">
+      <Card className="d-flex flex-row-reverse border-primary">
         <Card.Img
           variant="top"
           src={activity.ImagePath}
           style={{ width: "60vw", height: "60vh", objectFit: "cover" }}
           className="mx-auto"
         />
-        <Card.Body>
+        <Card.Body className="d-flex flex-column justify-content-center">
           <Card.Title>
-            <h1 className="text-center p-3">{activity.Name}</h1>
+            <h1 className="text-center pt-3">{activity.Name}</h1>
+            <div className="d-flex justify-content-center p-1 mb-3">
+              <Badge bg="primary">{activity.Type}</Badge>
+              <Badge bg="primary" className="mx-2">{activity.Price}</Badge>
+            </div>
           </Card.Title>
           <Card.Text>{activity.Description} </Card.Text>
-          <Card.Text>{activity.Price} </Card.Text>
           <Card.Text>{activity.Address} </Card.Text>
-          <Card.Text>{activity.Type} </Card.Text>
           <Link to={`/`}>
             <Button variant="primary">More activities</Button>
           </Link>
